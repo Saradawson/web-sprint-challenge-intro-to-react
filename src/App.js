3c040257e44b5c65676308a5e5cc202da204ccdb
unknown <saradawsone@gmail.com>
2022-05-20 10:00:16 -0700

    character initial state declared

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -7,7 +7,7 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [character, setCharacter] = useState();
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
